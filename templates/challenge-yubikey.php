fff221933062f3fbe4a1fd117483434f85debfbe
David Sexton <site-github@jack.org.uk>
2018-03-15 21:28:00 +0000

    Add screenshot.

--- templates/challenge-yubikey.php
+++ templates/challenge-yubikey.php
@@ -1,3 +1,6 @@
+<p>
+<?php p($l->t('Please plug in your Yubikey and press the button to authenticate.')) ?>
+</p>
 <form method="POST">
     <input type="password" name="challenge" required="required" autofocus autocomplete="off">
 </form>
