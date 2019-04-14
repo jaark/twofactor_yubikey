c651c657d420c80edbae89bfef337ef3ad193627
Jack <site-github@jack.org.uk>
2019-04-14 10:20:32 +0100

    Convert challenge input to text. #22

--- templates/challenge-yubikey.php
+++ templates/challenge-yubikey.php
@@ -2,5 +2,5 @@
 <?php p($l->t('Please plug in your Yubikey and press the button to authenticate.')) ?>
 </p>
 <form method="POST">
-    <input type="password" name="challenge" required="required" autofocus autocomplete="off">
+    <input type="text" name="challenge" required="required" autofocus autocomplete="off">
 </form>
